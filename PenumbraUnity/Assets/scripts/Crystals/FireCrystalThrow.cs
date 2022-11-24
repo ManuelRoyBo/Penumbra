20f4266e87f8c93309d87d611537bef03ffa6640
kingpin1905 <manuroyboisvert@hotmail.com>
2022-11-24 11:55:02 -0500

    Added script for Burning fire crystal
    Added script with a template for the code.
    
    Added timers with:
    1. Light Fade in
    2. Light hold
    3. Light fade out

--- PenumbraUnity/Assets/scripts/Crystals/FireCrystalThrow.cs
+++ PenumbraUnity/Assets/scripts/Crystals/FireCrystalThrow.cs
@@ -4,14 +4,62 @@ using UnityEngine;
 
 public class FireCrystalThrow : MonoBehaviour
 {
+    public UnityEngine.Rendering.Universal.Light2D Light;
+
+    public float MIN_INTENSITY = 0.0f; //Apparently, ou cannot make constants appear in the inspector. If you have an alternative, feel free to modify it.
+    public float DEFAULT_INTENSITY = 1.0f;
+    public float TIME_BEFORE_IGNITION = 2.0f;
+    public float IGNITE_DURATION = 1.0f;
+    public float BURN_DURATION = 5.0f;
+    public float FADE_TIME = 1.0f;
+
     // Start is called before the first frame update
     private void Start()
     {
+        /*
+         * 0. Crystal has just been thrown. Nothing really happens
+         * 1. Crystal ignites. Starts animation and lights up
+         * 2. Crystal is burning for a certain amount of time. Animation repeats
+         * 3. Crystal slowly fades until removed from the game. Light fades too.
+         */
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), GameManager.Instance.Player.GetComponent<Collider2D>(), true);
+
+        Light.intensity = MIN_INTENSITY;
+        StartCoroutine(Ignite(TIME_BEFORE_IGNITION));  //Ignition
+        StartCoroutine(Burn(BURN_DURATION + IGNITE_DURATION + TIME_BEFORE_IGNITION));  //burn
+        StartCoroutine(Fade(FADE_TIME+BURN_DURATION+ IGNITE_DURATION + TIME_BEFORE_IGNITION));  //Fade
     }
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator Ignite(float waitTime)
     {
-        
+        yield return new WaitForSeconds(waitTime);
+        //Ignition
+        Debug.Log(Mathf.PingPong(IGNITE_DURATION, DEFAULT_INTENSITY - MIN_INTENSITY));
+        Light.intensity = DEFAULT_INTENSITY;
+
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / IGNITE_DURATION)
+        {
+            Light.intensity = (Mathf.Lerp(MIN_INTENSITY, DEFAULT_INTENSITY, t));
+            yield return null;
+        }
     }
+    IEnumerator Burn(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //Burn
+    }
+    IEnumerator Fade(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //Fade
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / IGNITE_DURATION)
+        {
+            Light.intensity = (Mathf.Lerp(DEFAULT_INTENSITY,0, t));
+            yield return null;
+        }
+
+        //destroy
+    }
+
+
 }
