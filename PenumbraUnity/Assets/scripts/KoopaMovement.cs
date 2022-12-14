c78a323141f5c5e4778ba4d5c8f635a3f4bc1598
kingpin1905 <manuroyboisvert@hotmail.com>
2022-12-14 12:42:15 -0500

    Koopa modifications
    Koopa can now detect walls and turn around when it hits one.
    Koopa now isn't visible in shadows
    Koppa kills player on hitbox

--- PenumbraUnity/Assets/scripts/KoopaMovement.cs
+++ PenumbraUnity/Assets/scripts/KoopaMovement.cs
@@ -8,45 +8,55 @@ public class KoopaMovement : MonoBehaviour
     public float speed = 5f;
     [SerializeField] private LayerMask platformLayerMask;
     [SerializeField] private Rigidbody2D rb;
+    
+    [Header("Collider for ground")]
+    public BoxCollider2D collider;
     // Start is called before the first frame update
 
     bool isFacingRight = true;
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 rayDirection;
         if (isFacingRight)
         {
             rb.velocity = new Vector2(speed, rb.velocity.y);
-            rayDirection = new Vector2(1, -1);
         }
         else
         {
             rb.velocity = new Vector2(-speed, rb.velocity.y);
-            rayDirection = new Vector2(-1, -1);
         }
 
-        if (isNearEdge(rayDirection))
+        if (isNearEdge(isFacingRight) || isNearWall(isFacingRight))
         {
-            Debug.Log("isNearEdge");
             isFacingRight = !isFacingRight;
             gameObject.transform.localScale = new Vector2(-gameObject.transform.localScale.x, gameObject.transform.localScale.y);
         }
 
-        
 
-        
+
+
     }
-    private bool isNearEdge(Vector2 direction)
+    private bool isNearEdge(bool isFacingRight)
     {
-        float extraHeightText = 0.3f;
-        BoxCollider2D collider = gameObject.GetComponent<BoxCollider2D>();
-        RaycastHit2D raycastHit = Physics2D.Raycast(collider.bounds.center, direction,collider.bounds.extents.y + extraHeightText, platformLayerMask);
+        //direction of the rays
+        Vector2 rayDirection;
+        if (isFacingRight)
+        {
+            rayDirection = new Vector2(1, -1);
+        }
+        else
+        {
+            rayDirection = new Vector2(-1, -1);
+        }
+
+
+        float extraLengthText = 0.01f;
+        float rayCastLength = Mathf.Sqrt(Mathf.Pow(collider.size.y, 2f) + Mathf.Pow(collider.size.x, 2f)) + extraLengthText;
+        RaycastHit2D raycastHit = Physics2D.Raycast(collider.bounds.center, rayDirection, rayCastLength, platformLayerMask);
         Color rayColor;
         if (!(raycastHit.collider != null))
         {
@@ -56,7 +66,36 @@ public class KoopaMovement : MonoBehaviour
         {
             rayColor = Color.red;
         }
-        Debug.DrawRay(collider.bounds.center, direction * (collider.bounds.extents.y + extraHeightText),rayColor);
+        Debug.DrawRay(collider.bounds.center, rayDirection * (rayCastLength), rayColor);
         return !(raycastHit.collider != null);
+    }
+    private bool isNearWall(bool isFacingRight)
+    {
+        //direction of the rays
+        Vector2 rayDirection;
+        if (isFacingRight)
+        {
+            rayDirection = new Vector2(1, 0);
+        }
+        else
+        {
+            rayDirection = new Vector2(-1, 0);
+        }
+
+
+        float extraLengthText = 0.01f;
+        float rayCastLength = collider.size.x + extraLengthText;
+        RaycastHit2D raycastHit = Physics2D.Raycast(collider.bounds.center, rayDirection, rayCastLength, platformLayerMask);
+        Color rayColor;
+        if (!(raycastHit.collider != null))
+        {
+            rayColor = Color.green;
+        }
+        else
+        {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(collider.bounds.center, rayDirection * (rayCastLength), rayColor);
+        return (raycastHit.collider != null);
     }
 }
